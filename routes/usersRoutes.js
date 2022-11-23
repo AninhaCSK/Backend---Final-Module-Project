38dc754de70b3759f8478944b45d9b8cefb85465
AninhaCSK <acsk1982@gmail.com>
2022-11-23 16:15:33 +0100

    routes done

--- routes/usersRoutes.js
+++ routes/usersRoutes.js
@@ -5,15 +5,15 @@ import {
   addUser,
   deleteUser,
   getUsers,
+  getOneUser,
   updateUser,
 } from "../controllers/userControllers.js";
 
-import {signin,
-signup} from "../controllers/authController.js"
+import { signin, signup } from "../controllers/authController.js";
 
 router.route("/addUser").post(addUser);
 router.route("/getUsers").get(getUsers);
-router.route("/getOneUser").get(getUsers);
+router.route("/:id").get(getOneUser);
 router.route("/:id").put(updateUser);
 router.route("/:id").delete(deleteUser);
 router.route("/signin").post(signin);
